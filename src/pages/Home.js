4068269a5814e1cca512b21a1cf84b12633763b7
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-04 03:46:27 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,12 @@ import Footer from "../Footer";
 import Welcome from "../Header";
 import Hero from "../Hero";
 import HomeMenu from "../HomeMenu";
-import Navbar from "../Navbar";
+// import Navbar from "../Navbar";
 
 const Home = () => {
     return(
         <article>
-            <Navbar />  
+            {/* <Navbar />   */}
             <Welcome />
             <Hero />
             <FeaturedList />
