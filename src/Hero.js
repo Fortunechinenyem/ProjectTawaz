24923f23b12f852e79bf2e85b925165d9d848265
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-04 07:26:35 +0100

    update

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,9 @@ import logoh from './images/tawazhero.jpg';
 
 function Hero() {
   return (
-    <div className="d-flex g-5 mb-3">
-        <div className="w-25">
-            <h2>Enjoy Delicious Food at affordable prices</h2>
+    <div className="d-flex g-5 mb-3 containerh">
+        <div className="herotext1 w-25">
+            <h2 className="mb-4">Enjoy Delicious Food at affordable prices!</h2>
             <a href="mailto:sarahtawanyida@gmail.com" className="btn btn-warning">View Menu</a>
         </div>
       <div className="heroimg w-75">
