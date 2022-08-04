c467d33527fc61bd082117436cc33800af2dad70
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-04 04:00:23 +0100

    update

--- src/FeaturedList.js
+++ src/FeaturedList.js
@@ -5,7 +5,7 @@ function FeaturedList() {
   return (
     <div className="feature w-100 ms-auto mb-3">
     <div className="card">
-  <h5 className="card-header">Star Menu</h5>
+  <h4 className="card-header1 text-danger">Star Menu</h4>
   <div className="card-body">
     <h5 className="card-title">Northern Delight!</h5>
     <p className="card-text">We have a Variety of Northern Delicacies, customised specially for you.</p>
