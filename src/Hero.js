228cf8a3e544e9905348b00da18b611368b511fc
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-04 02:50:55 +0100

    update

--- src/Hero.js
+++ src/Hero.js
@@ -3,9 +3,16 @@ import logoh from './images/tawazhero.jpg';
 
 function Hero() {
   return (
-    <div className="">
+    <div className="d-flex">
+        <div className="w-25">
+            <h1>Enjoy Delicious Food at affordable prices</h1>
+            <a href="mailto:sarahtawanyida@gmail.com" className="btn btn-warning">View Menu</a>
+        </div>
+      <div className="heroimg w-75">
         <img src={logoh} className="" alt="logoh" />
     </div>
+    </div>
+    
   );
 }
 
