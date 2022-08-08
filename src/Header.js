6e72d39183a05bf04dc1f1b8f52417d55db3db4a
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-08 09:23:05 +0100

    update

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Welcome = () =>{
     return (
-        <div className="welcome mb-5 mt-5">
+        <div className="welcome mb-5 mt-5 w-25 mx-auto">
             <h1>TAWAZ</h1>
         </div>
     )
