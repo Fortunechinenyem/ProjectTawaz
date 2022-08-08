1203ccc2c864ff014673f20dae94291a2810c891
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-08 14:20:25 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import Footer from "../Footer";
 import Welcome from "../Header";
 import Hero from "../Hero";
 import HomeMenu from "../HomeMenu";
-import Navbar from "../Navbar";
+// import Navbar from "../Navbar";
 
 const Home = () => {
   return (
