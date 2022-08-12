14b42ec75bf21f65310de29d06cbb784022e1646
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-12 07:05:28 +0100

    update

--- src/Menubody.js
+++ src/Menubody.js
@@ -16,7 +16,7 @@ const MenuBody = () => {
         className="form-select form-select-lg mb-3"
         aria-label=".form-select-lg example"
       >
-        <option selected>Breakfast Series</option>
+        <option select="true">Breakfast Series</option>
         <option value="1">Tea with bread</option>
         <option value="2">Tea with bread and egg</option>
         <option value="3">Plantain with egg sauce and Sausages</option>
@@ -26,7 +26,7 @@ const MenuBody = () => {
         className="form-select form-select-lg mb-3"
         aria-label=".form-select-lg example"
       >
-        <option selected>Selense Series</option>
+        <option select="true">Selense Series</option>
         <option value="1">Spaghetti with fish sauce</option>
         <option value="2">Spaghetti and stir fry Vegetables</option>
         <option value="3">Boiled Yam with fish sauce/Stew/Vegetable</option>
@@ -35,7 +35,7 @@ const MenuBody = () => {
         className="form-select form-select-lg mb-3"
         aria-label=".form-select-lg example"
       >
-        <option selected>Pasteries</option>
+        <option select="true">Pasteries</option>
         <option value="1">Cakes</option>
         <option value="2">Small chops and meat</option>
         <option value="3">Doughnut</option>
@@ -44,7 +44,7 @@ const MenuBody = () => {
         className="form-select form-select-lg mb-3"
         aria-label=".form-select-lg example"
       >
-        <option selected>Drinks and Smoothies</option>
+        <option select="true">Drinks and Smoothies</option>
         <option value="1">Kunun Aya</option>
         <option value="2">Zobo</option>
         <option value="3">Customized Smoothie</option>
