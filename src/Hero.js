44affe3816e268f35621c6ddc6d361bf75dbf38d
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-15 06:11:56 +0100

    update

--- src/Hero.js
+++ src/Hero.js
@@ -1,19 +1,16 @@
 import React from "react";
-import background from "./images/meat.jpg";
+import Heroimage from "./images/figma.png";
 import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${background})`,
-        height: "80vh",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="herotext">
-        <h1 className="mb-4">Good Food, Tasty Food!</h1>
-        <Link to="/menu" className="btn btn-warning">
+    <div className="">
+      <div className="homehero mb-5 mx-auto">
+        <div className="heroimage">
+          <img src={Heroimage} alt={Hero.png} />
+        </div>
+
+        <Link to="/menu" className="btn btn-warning  mt-3mb-3">
           VIEW MENU
         </Link>
       </div>
