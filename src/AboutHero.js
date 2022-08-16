14d0797f83368e985e360532b6dedf0c4eae022c
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-16 08:04:18 +0100

    update

--- src/AboutHero.js
+++ src/AboutHero.js
@@ -4,7 +4,7 @@ import AH from "./images/hero5.png";
 const AboutHero = () => {
   return (
     <div className="abouthero w-75 mx-auto mt-5 mb-5">
-      <img src={AH} alt={AboutHero.jpg} className="abouthero" />;
+      <img src={AH} alt={AboutHero.jpg} className="abouthero" />
     </div>
   );
 };
