66e2bc3543994f25e5a6d67858f18dcada6058bb
Fortunechinenyem <fortunechinenyem@gmail.com>
2022-08-19 04:03:03 +0100

    update

--- src/Hero.js
+++ src/Hero.js
@@ -1,15 +1,23 @@
 import React from "react";
-import Heroimage from "./images/herohome.png";
+import { Link } from "react-router-dom";
+import Heroimage from "./images/hero10.png";
 
 function Hero() {
   return (
-    <div className="">
-      <div className="homehero mb-5 mt-5 mx-auto w-75">
-        <div className="heroimage">
+    <>
+      <div className="homehero mb-5 mt-5 d-flex w-75 mx-auto">
+        <div className="w-25">
+          <h2 className="herotext1">Tawaz</h2>
+          <h4>
+            Great <span className="herotext">Food, </span>Great
+            <span className="herotext"> Taste!</span>
+          </h4>
+        </div>
+        <div className="w-75">
           <img src={Heroimage} alt={Hero.png} />
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
