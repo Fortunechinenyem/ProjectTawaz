990939a395c7aa474021fd49553506e63eda9eef
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-05-18 03:54:07 +0100

    update

--- src/ContactBody.js
+++ src/ContactBody.js
@@ -1,40 +1,19 @@
-import React from "react";
+// import React from "react";
 
-const ContactBody = () => {
-  return (
-    <div className="m-4 mx-auto mt-5 mb-5 p-5 rounded shadow">
-      <div className=" form-floating mb-3">
-        <input
-          type="email"
-          className="form-control border-0 bg-light"
-          id="floatingInput"
-          placeholder="name@example.com"
-        />
-        <label for="floatingInput">Email Address</label>
-      </div>
-      <div className="form-floating mb-3">
-        <input
-          type="phonenumber"
-          className="form-control border-0 bg-light"
-          id="floatingInput"
-        />
-        <label for="floatingInput">Phone Number</label>
-      </div>
-      <div className="form-floating">
-        <textarea
-          className="form-control border-0 bg-light"
-          placeholder="Leave a comment here"
-          id="floatingTextarea"
-        ></textarea>
-        <label for="floatingTextarea">Comments</label>
-      </div>
-      <div className="w-75 text-center mx-auto">
-        <a href="https://wa.link/o688gi" className="btn btn-warning mt-3">
-          Submit
-        </a>
-      </div>
-    </div>
-  );
-};
+// const ContactBody = () => {
+//   return (
+//     <div className="m-4 mx-auto mt-5 mb-5 p-5 rounded shadow">
+//       <div className="mx-auto text-center">
+//         <h3>You've got questions?</h3>
+//         <h4>We're just a call or chat away!</h4>
+//       </div>
+//       <div className="w-75 text-center mx-auto">
+//         <a href="https://wa.link/o688gi" className="btn btn-warning mt-3">
+//           Chat
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default ContactBody;
+// export default ContactBody;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,46 @@
 import React from "react";
-import ContactBody from "../ContactBody";
-import ContactHero from "../Contacthero";
+// import ContactBody from "../ContactBody";
+// import ContactHero from "../Contacthero";
+import { FiPhoneCall } from "react-icons/fi";
+import { BsFillChatDotsFill } from "react-icons/bs";
 import Footer from "../Footer";
 import MenuPrompt from "../MenuPrompt";
+import styled from "styled-components";
+
+const PhoneLink = styled.a`
+  text-decoration: none;
+  color: #333;
+  font-size: 16px;
+  margin: 10px 0;
+  transition: color 0.3s ease-in-out;
+
+  &:hover {
+    color: #555;
+  }
+`;
 
 const Contact = () => {
   return (
     <article>
-      <ContactHero />
-      <ContactBody />
+      {/* <ContactHero />
+      <ContactBody /> */}
+      <div className="m-4 mx-auto mt-5 mb-5 p-5 rounded shadow">
+        <div className="mx-auto text-center">
+          <h3>You've got questions?</h3>
+          <h4>We're just a call or chat away!</h4>
+        </div>
+        <div className=" text-center mx-auto">
+          <PhoneLink href="tel:+2348071715005" className="btn btn-warning mt-3">
+            <FiPhoneCall />
+            08071715005
+          </PhoneLink>
+        </div>
+        <div className=" text-center mx-auto">
+          <a href="https://wa.link/o688gi" className="btn btn-warning mt-3">
+            <BsFillChatDotsFill /> Chat
+          </a>
+        </div>
+      </div>
       <MenuPrompt />
       <Footer />
     </article>
